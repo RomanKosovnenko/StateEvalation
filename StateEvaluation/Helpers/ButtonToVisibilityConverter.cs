177f5ad278d68eb435dde98b3406ef2469600a85
Andriy2 <andruha.bakay1997@gmail.com>
2017-12-04 21:22:24 +0200

    biocolor hiding

--- StateEvaluation/Helpers/ButtonToVisibilityConverter.cs
+++ StateEvaluation/Helpers/ButtonToVisibilityConverter.cs
@@ -4,19 +4,36 @@ using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using StateEvaluation.ViewModel;
+using System.Windows.Controls;
+using System.Windows;
 
 namespace StateEvaluation.Helpers
 {
-    public class ButtonToVisibilityConverter : IValueConverter
+    public class ButtonToVisibilityConverter : IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            string input = value?.ToString();
-            return System.Windows.Visibility.Collapsed;
-
+            try
+            {
+                MessageBox.Show(values[0].ToString());
+                bool? masterVisible = null;
+                bool? slaveVisible = null;
+                if (masterVisible == true && slaveVisible == true)
+                {
+                    return Visibility.Visible;
+                }
+                else
+                {
+                    return Visibility.Hidden;
+                };
+            }
+            catch
+            {
+                return Visibility.Hidden;
+            }
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
