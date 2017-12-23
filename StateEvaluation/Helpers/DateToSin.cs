c0bbfc044a0bfa6d6b7252ec4bf707097a85581b
Andriy2 <andruha.bakay1997@gmail.com>
2017-12-23 15:55:20 +0200

    Починив своє те, що зламав Віталя

--- StateEvaluation/Helpers/DateToSin.cs
+++ StateEvaluation/Helpers/DateToSin.cs
@@ -10,20 +10,25 @@ namespace StateEvaluation.Helpers
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             //    DateTime nw = DateTime.Today;
-            if (value == null)
+            string userid = value.ToString().Trim();
+
+
+            MainWindow.people.TryGetValue(userid, out string birthday);
+
+            if (birthday == null || birthday.Trim().Length == 0)
             {
-                return "--";
+                return "---";
             }
 
             DateTime nw = DateTime.Now;
-            DateTime dt = (DateTime) value;
+            DateTime dt = System.Convert.ToDateTime(birthday);
             double Delta = (int)((nw.Subtract(dt)).TotalDays);
 
             int.TryParse(parameter.ToString(), out int DAYS);
 
             double sta = -Math.Sin((0.0 + Delta / DAYS * 360) / 180 * Math.PI);
             double now = -Math.Sin((0.1 + Delta / DAYS * 360) / 180 * Math.PI);
-            return now > sta ? "↗":"↘";
+            return String.Format("{0} ({1})", now > sta ? "↗" : "↘", (int)Delta % DAYS);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
