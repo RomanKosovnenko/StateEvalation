89c6ba063896f64ca75f954316825f12588004bf
Vitalii Kushch <Vitalii.Kushch@infopulse.com>
2018-11-20 16:21:16 +0200

    init unit tests project

---
+++ UnitTests/Tests/Biocolor/ImageGenerator.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ImageGenerator
+    {
+        [Fact(DisplayName = "SE: Biocolor ImageGenerator RgbToCmyk")]
+        public void RgbToCmyk()
+        {
+            //set var
+
+            //action
+
+            //checked result
+
+        }
+    }
+}
