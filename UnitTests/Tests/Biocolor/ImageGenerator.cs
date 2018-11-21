9cb00b11a17b9375ccf4623130a4a4f15b1493ed
Vitalii Kushch <Vitalii.Kushch@infopulse.com>
2018-11-21 18:45:18 +0200

    first example test

--- UnitTests/Tests/Biocolor/ImageGenerator.cs
+++ UnitTests/Tests/Biocolor/ImageGenerator.cs
@@ -1,18 +1,23 @@
 using System;
 using Xunit;
+using StateEvaluation.BioColor;
 
 namespace UnitTests
 {
-    public class ImageGenerator
+    public class ImageGeneratorTests
     {
         [Fact(DisplayName = "SE: Biocolor ImageGenerator RgbToCmyk")]
         public void RgbToCmyk()
         {
             //set var
+            string RGB = "test color";
+            int[] expectResult = new int[1] ;
 
             //action
+            var result = ImageGenerator.RgbToCmyk(RGB);
 
             //checked result
+            Assert.Equal(expectResult, result);
 
         }
     }
